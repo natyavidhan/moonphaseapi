80c5c5c1a41279f9b31363f426cd981ecb366f22
studiousgamer <natyavidhanbiswas10@gmail.com>
2023-08-24 23:45:34 +0530

    scraped

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import requests
 from flask import Flask, jsonify, request
 import json
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
+
+countries = json.load(open("countries.json"))
 
 def get_data(country, state, date, month, year):
+    if country.title() not in countries or state not in countries[country.title()]:
+        return {
+            "error": "Country/state not found"
+        }
     data = requests.get(f"https://phasesmoon.com/{country}/{state}/moonday{date}{month}{year}.html").text
+    with open("temp.html", "w", encoding="utf-8") as f:
+        f.write(data)
     soup = BeautifulSoup(data, 'html.parser')
 
     moon_phase = soup.select_one("body > div.container > div.row.headerdetails > div:nth-child(1) > ul > li.phasename > strong").string
@@ -18,16 +27,20 @@ def get_data(country, state, date, month, year):
     moon_age = soup.select_one("body > div.container > div:nth-child(4) > table > tbody > tr:nth-child(5) > td:nth-child(2)").string
     moon_angle = soup.select_one("body > div.container > div:nth-child(4) > table > tbody > tr:nth-child(6) > td:nth-child(2)").string
     moon_distance = soup.select_one("body > div.container > div:nth-child(4) > table > tbody > tr:nth-child(7) > td:nth-child(2)").string
+    moon_img = soup.select_one("body > div.container > div.row.headerdetails > div.col4.text-center > div > img")["src"]
 
     return {
         "phase": moon_phase,
         "rise": moon_rise,
         "set": moon_set,
-        "duration": moon_duration,
-        "visibility": moon_visibility,
+        "duration": timedelta(hours=int(moon_duration.split(" ")[0]), minutes=int(moon_duration.split(" ")[3])).total_seconds(),
+        "visibility": float(moon_visibility),
         "constellation": moon_constellation,
         "horoscope": moon_horoscope,
         "age": moon_age,
-        "angle": moon_phase,
+        "angle": moon_angle,
         "distance": moon_distance,
+        "img": moon_img
     }
+
+# print(get_data("india", "new-delhi", "22", "october", "2023"))
--- scrape.py
+++ scrape.py
@@ -1,1 +1,30 @@
-link = "https://phasesmoon.com/countries/"
+import requests
+from bs4 import BeautifulSoup
+
+countries_ = {}
+
+for _ in ["", "2"]:
+    data = requests.get(f"https://phasesmoon.com/countries{_}/").text
+    soup = BeautifulSoup(data, 'html.parser')
+    countries = soup.select("body > div.container > div.cols > div")
+    for i in countries:
+        # print(i.img)
+        states_ = []
+        last = -1
+        n=1
+        while last !=0:
+            last=0
+            data2 = requests.get(i.a['href']).text if n == 1 else requests.get(i.a['href'][:-1]+str(n)).text
+            soup2 = BeautifulSoup(data2, 'html.parser')
+            states = soup2.select("body > div.container > div.cols > div")
+            for j in states:
+                if j.img == None:
+                    last+=1
+                states_.append(j.div.a["href"].replace("https://phasesmoon.com/", "").split("/")[1])
+            n+=1
+        countries_[i.a.string] = states_
+        print(f"{i.a.string}: {len(states_)} states")
+
+import json
+
+json.dump(countries_, open("countries.json", "w", encoding="utf-8"))
